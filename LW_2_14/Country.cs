8f25dd7865b898c5baff1f681a924411110f4e5b
VadimKarelov <vadim.karelov.02@mail.ru>
2022-02-23 14:09:57 +0500

    Created Country and Continent class

---
+++ LW_2_14/Country.cs
@@ -0,0 +1,40 @@
+﻿using LW_2_13;
+
+namespace LW_2_14
+{
+    public  class Country
+    {
+        public List<Organization> OrganizationList;
+
+        public Country()
+        {
+            OrganizationList = new List<Organization>();
+        }
+
+        public Country(int count, Random rn)
+        {
+            OrganizationList = new List<Organization>();
+            for (int i = 0; i < count; i++)
+            {
+                switch (rn.Next(0, 5))
+                {
+                    case 0: OrganizationList.Add(new Organization(ref rn)); break;
+                    case 1: OrganizationList.Add(new Factory(ref rn)); break;
+                    case 2: OrganizationList.Add(new InsuranceCompany(ref rn)); break;
+                    case 3: OrganizationList.Add(new Library(ref rn)); break;
+                    case 4: OrganizationList.Add(new ShipConstructingCompany(ref rn)); break;
+                }
+            }
+        }
+
+        public string Show()
+        {
+            string res = "";
+            foreach (var item in OrganizationList)
+            {
+                res += item.ToString() + '\n';
+            }
+            return res;
+        }
+    }
+}
---
+++ LW_2_14/Program.cs
@@ -0,0 +1,15 @@
+﻿using LW_2_13;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LW_2_14
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
