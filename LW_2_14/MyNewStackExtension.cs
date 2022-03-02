5649e45b2af69fe98b558c7f1476e2eebbdca3da
VadimKarelov <vadim.karelov.02@mail.ru>
2022-03-02 17:31:18 +0500

    Write demonstration program for part 2 & some bug fixes

--- LW_2_14/MyNewStackExtension.cs
+++ LW_2_14/MyNewStackExtension.cs
@@ -21,5 +21,54 @@ namespace LW_2_14
             }
             return res;
         }
+
+        public static int Count<T>(this MyNewStack<T> ms, Func<T, bool> predicate)
+        {
+            int res = 0;
+            foreach (var item in ms)
+            {
+                if (predicate(item))
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+
+        public static double Average<T>(this MyNewStack<T> ms)
+        {
+            if (ms is MyNewStack<Organization> msO)
+            {
+                return msO.Average(x => x.AverageSalary);
+            }
+            return ms.Average();
+        }
+
+        public static double Min<T>(this MyNewStack<T> ms)
+        {
+            if (ms is MyNewStack<Organization> msO)
+            {
+                return msO.Min(x => x.AverageSalary);
+            }
+            return ms.Min();
+        }
+
+        public static double Max<T>(this MyNewStack<T> ms)
+        {
+            if (ms is MyNewStack<Organization> msO)
+            {
+                return msO.Max(x => x.AverageSalary);
+            }
+            return ms.Max();
+        }
+
+        public static double Sum<T>(this MyNewStack<T> ms)
+        {
+            if (ms is MyNewStack<Organization> msO)
+            {
+                return msO.Sum(x => x.AverageSalary);
+            }
+            return ms.Sum();
+        }
     }
 }
--- LW_2_14/Program.cs
+++ LW_2_14/Program.cs
@@ -7,33 +7,32 @@ namespace LW_2_14
 {
     public class Program
     {
+        private static Random rn = new();
+
         public static void Main(string[] args)
         {
-            Random rn = new();
+            Part1();
+            Part2();
+        }
 
+        private static void Part1()
+        {
             List<Continent> l = new();
-            for (int i = 0; i < 2; i++) l.Add(new Continent(5, rn));
+            for (int i = 0; i < 3; i++) l.Add(new Continent(5, rn));
 
             Print(l);
 
+            Console.WriteLine("Организации с зарплатой выше 9000");
             Print(Request1(l));
 
-            Console.WriteLine("\nTotal number of books:" + Request2(l));
+            Console.WriteLine("\nВсего книг во всех библиотеках:" + Request2(l));
 
-            Console.WriteLine($"\nOrganizations with salary over 9000: {Request3(l)}\n");
+            Console.WriteLine($"\nКоличество организаций с зарплатой больше 9000: {Request3(l)}\n");
 
+            Console.WriteLine("Все организации из Magadan");
             Print(Request4(l));
 
-            Console.WriteLine($"\nInsurance with number of clients over 80: {Request5(l)}\n");
-
-            MyNewStack<int> st = new();
-            st.Push(10);
-            st.Push(15);
-            st.Push(10);
-
-            Console.WriteLine(st.Sum(x => x));
-            st.Select(x => x == 5);
-            Print(st.Select(x => x == 10));
+            Console.WriteLine($"\nКоличество страховых с количеством клиентов больше 80: {Request5(l)}\n");
         }
 
         private static List<Organization> Request1(List<Continent> continents)
@@ -65,7 +64,8 @@ namespace LW_2_14
             // получение счетчика методами расширения
             // подсчет количества организаций с зарплатой выше 9000
 
-            return continents.Select(continent => continent.Countries.Select(country => country.OrganizationList.Values.ToList().Where(org => org.AverageSalary > 9000))).Count();
+            return continents.Sum(continent => continent.Countries.Sum(
+                country => country.OrganizationList.Values.ToList().Where(org => org.AverageSalary > 9000).Count()));
         }
 
         private static List<string> Request4(List<Continent> continents)
@@ -90,6 +90,33 @@ namespace LW_2_14
                     from org in country.OrganizationList.Values
                     where org is InsuranceCompany ins && ins.NumberOfClients > 80
                     select org).Count();
+        }
+
+        private static void Part2()
+        {
+            Console.WriteLine("<<<Часть 2>>>");
+
+            MyNewStack<Organization> st = new();
+
+            for (int i = 0; i < 10; i++)
+            {
+                switch (rn.Next(0, 5))
+                {
+                    case 0: var org0 = new Organization(ref rn); st.Push(org0); break;
+                    case 1: var org1 = new Factory(ref rn); st.Push(org1); break;
+                    case 2: var org2 = new Library(ref rn); st.Push(org2); break;
+                    case 3: var org3 = new InsuranceCompany(ref rn); st.Push(org3); break;
+                    case 4: var org4 = new ShipConstructingCompany(ref rn); st.Push(org4); break;
+                }
+            }
+
+            Print(st);
+
+            Console.WriteLine($"\nКоличество элементов {st.Count()}");
+            Console.WriteLine($"Средняя зарплата {st.Average()}");
+            Console.WriteLine($"Минимальная зарплата {st.Min()}");
+            Console.WriteLine($"Максимальная зарплата {st.Max()}");
+            Console.WriteLine($"Сумма всех зарплат {st.Sum()}");
         }
 
         private static void Print<T>(IEnumerable<T> collection)
